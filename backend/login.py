666e1b7a40496067217b547777f7ce1b128bc203
Mahesh-Pappu <140877484+Mahesh-Pappu@users.noreply.github.com>
2024-07-08 20:18:59 +0530

    Add files via upload

---
+++ backend/login.py
@@ -0,0 +1,18 @@
+from sql_connection import get_sql_connection
+def verify_user(connection, email, password):
+    cursor=connection.cursor()
+    query = "SELECT * FROM users WHERE email = %s AND password = %s;"
+    cursor.execute(query, (email,password))
+    user = cursor.fetchone()
+    cursor.close()
+
+    return user
+if __name__== '__main__':
+    connection=get_sql_connection()
+    a=input("Enter email :")
+    b=input("Enter password: ")
+    user=verify_user(connection,a,b)
+    if(user):
+        print("Logged in")
+    else:
+        print("Invalid Credentials")
