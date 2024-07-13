578f29e8b16e005ceaa9988b2a982186d645c297
Mahesh-Pappu <140877484+Mahesh-Pappu@users.noreply.github.com>
2024-07-13 16:48:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-import os
+from show_and_track_past_orders import get_customer_orders, get_order_id, get_order_status
+from orders import main1
+from connection import get_sql_connection
 
 def main():
     print("Welcome to GroceryMart!")
 
-    customer_id = input("Please enter your customer ID: ")
+    customer_id = int(input("Please enter your customer ID: "))
 
     print("What would you like to do?")
     print("a) Order")
@@ -11,15 +13,23 @@ def main():
     print("c) Show Previous Orders")
 
     choice = input("Enter your choice (a/b/c): ").lower()
-
     if choice == 'a':
-        os.system(f"python order_page.py {customer_id}")
+        main1(customer_id)
     elif choice == 'b':
-        os.system("python track_order.py")
+        connection=get_sql_connection()
+        print("Here are your orders :")
+        get_order_id(customer_id,connection)
+        print("-" * 20)
+        order_id = int(input("Enter the order_id you want to track :"))
+        get_order_status(order_id,connection)
+        connection.close()
     elif choice == 'c':
-        os.system(f"python show_prev_order.py")
+        connection=get_sql_connection()
+        get_customer_orders(customer_id,connection)
+        connection.close()
     else:
         print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
